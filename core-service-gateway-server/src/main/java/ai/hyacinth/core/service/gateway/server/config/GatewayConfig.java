5842f331d6e410101f23f4d7092f7d298d37b550
Xu Yijun <xuyijun@gmail.com>
2019-04-21 23:57:59 +0800

    fix ComponentScan on rate-limiter

--- core-service-gateway-server/src/main/java/ai/hyacinth/core/service/gateway/server/config/GatewayConfig.java
+++ core-service-gateway-server/src/main/java/ai/hyacinth/core/service/gateway/server/config/GatewayConfig.java
@@ -3,6 +3,7 @@ package ai.hyacinth.core.service.gateway.server.config;
 import ai.hyacinth.core.service.discovery.support.config.DiscoveryConfig;
 import ai.hyacinth.core.service.gateway.server.configprops.GatewayServerProperties;
 import ai.hyacinth.core.service.gateway.server.jwt.JwtService;
+import ai.hyacinth.core.service.gateway.server.ratelimiter.SimpleRateLimiter;
 import ai.hyacinth.core.service.gateway.server.web.GatewayController;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +12,12 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({DiscoveryConfig.class, RouteConfig.class, SecurityConfig.class})
-@ComponentScan(basePackageClasses = {GatewayConfig.class, GatewayController.class, JwtService.class})
+@ComponentScan(
+    basePackageClasses = {
+      GatewayConfig.class,
+      GatewayController.class,
+      JwtService.class,
+      SimpleRateLimiter.class
+    })
 @EnableConfigurationProperties({GatewayServerProperties.class})
-public class GatewayConfig {
-}
+public class GatewayConfig {}
