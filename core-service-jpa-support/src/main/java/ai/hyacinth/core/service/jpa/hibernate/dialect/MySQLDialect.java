4fc8fd9ed7bfe2d1d5ef6d4a02bb8f70e5ff34b2
Xu Yijun <xuyijun@gmail.com>
2019-10-22 23:27:54 +0800

    to support local deploy

--- core-service-jpa-support/src/main/java/ai/hyacinth/core/service/jpa/hibernate/dialect/MySQLDialect.java
+++ core-service-jpa-support/src/main/java/ai/hyacinth/core/service/jpa/hibernate/dialect/MySQLDialect.java
@@ -2,6 +2,10 @@ package ai.hyacinth.core.service.jpa.hibernate.dialect;
 
 import org.hibernate.dialect.MySQL57Dialect;
 
+@Deprecated
+/**
+ * could be replaced by "org.hibernate.dialect.MySQL57InnoDBDialect"
+ */
 public class MySQLDialect extends MySQL57Dialect {
   @Override
   public String getTableTypeString() {
